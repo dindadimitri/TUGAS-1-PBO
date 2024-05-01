9a4fb36ab3e394dd00903e7bed9c59e00b63e2c2
dindadimitri <147127252+dindadimitri@users.noreply.github.com>
2024-05-01 23:18:21 +0800

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,34 @@
+package pbotugas1;
+
+
+
+public class Menu {
+    String name;
+    double price;
+
+    public Menu (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return "Menu : " +
+                name + " || Rp." + price;
+    }
+}
